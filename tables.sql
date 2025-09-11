c1f5313fefd6f41dd12d298811cf3392c43b0a2c
Shehzail Abbas <abbasshehzail123@gmail.com>
2025-09-11 11:59:16 +0530

    fixed

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS companydb;
+CREATE DATABASE IF NOT EXISTS company_db;
 USE companydb;
 
 SET FOREIGN_KEY_CHECKS = 0;
@@ -11,14 +11,12 @@ CREATE TABLE Employee (
     Job_role VARCHAR(100) NOT NULL,
     Email VARCHAR(100) UNIQUE NOT NULL,
     EmployeeStatus VARCHAR(50) NOT NULL,
-    Manager_id INT DEFAULT NULL,
-    FOREIGN KEY (Manager_id) REFERENCES Employee(Staff_id) ON DELETE SET NULL
+    Manager_id INT DEFAULT NULL
 );
 
 DROP TABLE IF EXISTS Manager;
 CREATE TABLE Manager(
-    Staff_id INT NOT NULL,
-    FOREIGN KEY (Staff_id) REFERENCES Employee(Staff_id)
+    Staff_id INT NOT NULL
 );
 
 DROP TABLE IF EXISTS projects;
@@ -36,13 +34,12 @@ CREATE TABLE projects (
 
 DROP TABLE IF EXISTS projects_under_employee;   
 CREATE TABLE projects_under_employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     StartDate DATE,
     EndDate DATE,
     ProjectId INT,
     Staff_id INT,
-    Project_status ENUM('Not Started', 'In Progress', 'Completed'),
-    FOREIGN KEY (Staff_id) REFERENCES Employee(Staff_id),
-    FOREIGN KEY (ProjectId) REFERENCES projects(ProjectId)
+    Project_status ENUM('Not Started', 'In Progress', 'Completed')
 );
 
  
@@ -51,9 +48,7 @@ DROP TABLE IF EXISTS project_members;
 CREATE TABLE project_members (
     ProjectId INT,
     Staff_id INT,
-    AssignedDate DATE,
-    FOREIGN KEY (ProjectId) REFERENCES projects(ProjectId),
-    FOREIGN KEY (staff_id) REFERENCES employee(staff_id)
+    AssignedDate DATE
 );
 
 DROP TABLE IF EXISTS users;
@@ -66,4 +61,3 @@ CREATE TABLE users (
     Password_hash VARCHAR(255) NOT NULL,
     Job_role ENUM('employee', 'manager') NOT NULL
 );
-
